fc29f0742e61ff7a41d548fec31c5a24ebd6616b
Abubakar Abid <abubakar@huggingface.co>
2023-07-25 01:43:07 +0300

    fix

--- docs/build/bundle.js
+++ docs/build/bundle.js
@@ -1051,9 +1051,9 @@ var app = (function () {
 
   function isHexCode(c) {
     return (
-      (0x30 /* 0 */ <= c && c <= 0x39 /* 9 */) ||
-      (0x41 /* A */ <= c && c <= 0x46 /* F */) ||
-      (0x61 /* a */ <= c && c <= 0x66 /* f */)
+      (0x30 /* 0 */ <= c && c <= 0x39) /* 9 */ ||
+      (0x41 /* A */ <= c && c <= 0x46) /* F */ ||
+      (0x61 /* a */ <= c && c <= 0x66) /* f */
     );
   }
 
@@ -3905,8 +3905,8 @@ var app = (function () {
     var cIsNsCharOrWhitespace = isNsCharOrWhitespace(c);
     var cIsNsChar = cIsNsCharOrWhitespace && !isWhitespace(c);
     return (
-      (// ns-plain-safe
-      (inblock // c = flow-in
+      // ns-plain-safe
+      ((inblock // c = flow-in
         ? cIsNsCharOrWhitespace
         : cIsNsCharOrWhitespace &&
           // - c-flow-indicator
