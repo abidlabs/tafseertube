535d08491d6b841cf4fb27977d69a5038067dfad
Abubakar Abid <abubakar@huggingface.co>
2023-07-26 03:59:53 +0300

    count

---
+++ count.py
@@ -0,0 +1,23 @@
+import json
+import yaml
+
+surahs = json.load(open('src/surahs.json'))
+videos = yaml.safe_load(open('docs/videos.yaml'))["videos"]
+
+ayahs = 0
+ayahs_with_videos = 0
+
+for s, surah in enumerate(surahs):
+    for i in range(surah["ayahs"]):
+        ayahs += 1
+        for video in videos:
+            surah = str(video["verses"]).split(":")[0]
+            ayah = str(video["verses"]).split(":")[1]
+            start_ayah = ayah.split("-")[0]
+            end_ayah = ayah.split("-")[1] if len(ayah.split("-")) > 1 else start_ayah
+            if surah == str(s + 1) and int(start_ayah) <= i + 1 <= int(end_ayah):
+                ayahs_with_videos += 1
+
+
+print("There are " + str(ayahs) + " ayahs in the Quran.")
+print("Of them, " + str(ayahs_with_videos) + " have videos.")
